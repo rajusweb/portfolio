2b640b35219d5c3b4be95a914961e3784d6ea722
RAJU KUMAR <rajukumarbth096@gmail.com>
2024-06-22 23:03:01 +0530

    Add files via upload

---
+++ Fan_Project.js
@@ -0,0 +1,19 @@
+const fan = document.getElementById("fan");
+
+function ONfun()
+{fan.style.animationDuration= 0.6+"s";}
+
+function OFFfun()
+{fan.style.animationDuration= 0+"s";}
+
+function fun1()
+{fan.style.animationDuration= 0.5+"s";}
+
+function fun2()
+{fan.style.animationDuration= 0.4+"s";}
+
+function fun3()
+{fan.style.animationDuration= 0.3+"s";}
+
+function fun4()
+{fan.style.animationDuration= 0.2+"s";}
